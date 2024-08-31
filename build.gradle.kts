824d651e7b5999f3cea265248afcabe5de0d4293
Jaehyeon <wogusn@gmail.com>
2024-08-31 15:05:59 +0900

    chore: 의존성 버전을 한 곳에서 관리

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,72 +1,76 @@
 plugins {
-	kotlin("jvm") version "1.9.25"
-	kotlin("plugin.spring") version "1.9.25"
-	id("org.springframework.boot") version "3.3.3"
-	id("io.spring.dependency-management") version "1.1.6"
-	kotlin("plugin.jpa") version "1.9.25"
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.3.3"
+    id("io.spring.dependency-management") version "1.1.6"
+    kotlin("plugin.jpa") version "1.9.25"
 }
+
+// 버전 관리
+val versions = mapOf(
+    "mysqlConnector" to "8.0.26",
+    "awsSpringCloud" to "2.4.4",
+    "java" to "17"
+)
 
 group = "com.numberone"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(versions["java"]!!) // non-null
+    }
 }
 
 configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	// ====================================== prod ======================================
-	// security
-	//implementation("org.springframework.boot:spring-boot-starter-security") todo 시큐리티 적용
+    // ====================================== prod ======================================
+    // security
+    //implementation("org.springframework.boot:spring-boot-starter-security") todo 시큐리티 적용
 
-	// db
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-jdbc")
-	implementation("mysql:mysql-connector-java:8.0.26")
+    // db
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation("mysql:mysql-connector-java:${versions["mysqlConnector"]}")
 
-	// web
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    // web
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
-	// kotlin
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
+    // kotlin
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
 
-	// aws cloud
-	implementation("io.awspring.cloud:spring-cloud-starter-aws-secrets-manager-config:2.4.4")
+    // aws cloud
+    implementation("io.awspring.cloud:spring-cloud-starter-aws-secrets-manager-config:${versions["awsSpringCloud"]}")
 
-	compileOnly("org.projectlombok:lombok")
-	annotationProcessor("org.projectlombok:lombok")
+    // ====================================== test ======================================
+    // security
+    testImplementation("org.springframework.security:spring-security-test")
 
-	// ====================================== test ======================================
-	// security
-	testImplementation("org.springframework.security:spring-security-test")
+    // spring
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 
-	// spring
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-
-	// junit
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    // junit
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict")
-	}
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
