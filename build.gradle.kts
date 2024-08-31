affeee0c5f6d06e7ada79fe1b2219f8d9d31dbda
Jaehyeon <wogusn@gmail.com>
2024-09-01 00:37:15 +0900

    chore: jpa 지연 로딩을 위한 어노테이션 기반 open 처리

--- build.gradle.kts
+++ build.gradle.kts
@@ -78,3 +78,9 @@ kotlin {
 tasks.withType<Test> {
     useJUnitPlatform()
 }
+
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
+}
