b535fc5f7ca220b1bdda834d1beeae1ea2285ffe
Jaehyeon <wogusn@gmail.com>
2024-08-31 16:47:02 +0900

    feat: 예외 별로 log level 을 구분

--- src/main/kotlin/com/numberone/daepiro/global/exception/CustomErrorContext.kt
+++ src/main/kotlin/com/numberone/daepiro/global/exception/CustomErrorContext.kt
@@ -5,8 +5,13 @@ import org.springframework.http.HttpStatus
 enum class CustomErrorContext(
     val code: Int,
     val message: String,
+    val logLevel: LogLevel = LogLevel.DEBUG
 ) {
     USER_NAME_DUPLICATED(HttpStatus.BAD_REQUEST.value(), "이미 중복된 이름의 회원이 존재합니다."),
 
-    UNCAUGHT_ERROR(999, "예기치 않은 오류가 발생하였습니다.")
+    UNCAUGHT_ERROR(999, "예기치 않은 오류가 발생하였습니다.", LogLevel.ERROR)
+}
+
+enum class LogLevel {
+    ERROR, DEBUG
 }
--- src/main/kotlin/com/numberone/daepiro/global/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/numberone/daepiro/global/exception/GlobalExceptionHandler.kt
@@ -1,31 +1,42 @@
 package com.numberone.daepiro.global.exception
 
+import mu.KotlinLogging
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.context.request.RequestContextHolder
 import org.springframework.web.context.request.ServletRequestAttributes
 
+private val logger = KotlinLogging.logger { }
+
 @RestControllerAdvice
 class GlobalExceptionHandler {
     @ExceptionHandler(CustomException::class)
     fun handleCustomException(exception: CustomException): ResponseEntity<CustomErrorResponse> {
-        return handle(exception.context)
+        return handle(exception.context, exception, extractEndpoint())
     }
 
     @ExceptionHandler(Exception::class)
     fun handleUnCaughtException(exception: Exception): ResponseEntity<CustomErrorResponse> {
-        println(exception.message)
-        return handle(CustomErrorContext.UNCAUGHT_ERROR)
+        return handle(CustomErrorContext.UNCAUGHT_ERROR, exception, extractEndpoint())
     }
 
-    private fun handle(context: CustomErrorContext): ResponseEntity<CustomErrorResponse> {
-        val endpoint = getEndpoint()
+    private fun handle(
+        context: CustomErrorContext,
+        exception: Throwable,
+        endpoint: String
+    ): ResponseEntity<CustomErrorResponse> {
         val response = CustomErrorResponse.of(context, endpoint)
+
+        when (context.logLevel) {
+            LogLevel.DEBUG -> logger.debug(exception) { "Handling at $endpoint" }
+            LogLevel.ERROR -> logger.error(exception) { "Handling at $endpoint" }
+        }
+
         return ResponseEntity.status(response.code).body(response)
     }
 
-    private fun getEndpoint(): String {
+    private fun extractEndpoint(): String {
         // 현재 쓰레드의 요청 정보(HTTP 요청에 대한 메서드, 경로, 헤더 등) 를 가져온 뒤 ServletRequestAttributes 으로 캐스팅한다.
         val requestAttributes = RequestContextHolder.getRequestAttributes() as? ServletRequestAttributes
         val httpRequest = requestAttributes?.request
