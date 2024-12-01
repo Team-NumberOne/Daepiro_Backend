466d66501c119538ac0a942bb5afec1f023f1fe1
versatile0010 <wogusn@gmail.com>
2024-12-01 21:50:55 +0900

    feat: remove deprecated

--- src/main/kotlin/com/numberone/daepiro/domain/file/repository/FileRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.numberone.daepiro.domain.file.repository
-
-import com.numberone.daepiro.domain.file.entity.FileEntity
-import org.springframework.data.jpa.repository.JpaRepository
-
-@Deprecated("Deprecated. File 관련 엔드포인트를 분리하지 않는다.")
-interface FileRepository : JpaRepository<FileEntity, Long>
