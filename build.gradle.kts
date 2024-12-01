85098e1d0dfc132d0c5e1a47820b9e5016c0d2b6
versatile0010 <wogusn@gmail.com>
2024-12-01 22:00:30 +0900

    chore: remove ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,6 @@ plugins {
     kotlin("plugin.jpa") version "1.9.25"
     id("org.springframework.boot") version "3.3.3"
     id("io.spring.dependency-management") version "1.1.6"
-    id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
-    id("org.jlleitschuh.gradle.ktlint-idea") version "10.3.0"
 }
 
 // 버전 관리
